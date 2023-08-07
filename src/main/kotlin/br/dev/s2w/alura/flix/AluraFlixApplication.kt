3cc9bf975af2c6fed026d7ca7f2dfbd026e82cb1
WybsonSantana <wybson@hotmail.com>
2023-08-06 21:49:56 -0300

    Create project

---
+++ src/main/kotlin/br/dev/s2w/alura/flix/AluraFlixApplication.kt
@@ -0,0 +1,11 @@
+package br.dev.s2w.alura.flix
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AluraFlixApplication
+
+fun main(args: Array<String>) {
+	runApplication<AluraFlixApplication>(*args)
+}
---
+++ src/test/kotlin/br/dev/s2w/alura/flix/AluraFlixApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.dev.s2w.alura.flix
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AluraFlixApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
