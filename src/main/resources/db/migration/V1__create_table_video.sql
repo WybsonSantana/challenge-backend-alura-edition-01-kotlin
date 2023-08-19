9be6e7b146688c19014691d61b69d2416df55b6b
WybsonSantana <wybson@hotmail.com>
2023-08-18 22:27:43 -0300

    Add FlyWay

---
+++ src/main/resources/db/migration/V1__create_table_video.sql
@@ -0,0 +1,7 @@
+CREATE TABLE video(
+    id int NOT NULL AUTO_INCREMENT,
+    titulo varchar(255) NOT NULL,
+    descricao varchar(255) NOT NULL,
+    url varchar(255) NOT NULL,
+    PRIMARY KEY(id)
+);
--- src/main/resources/db/migration/V2__insert_into_table_video.sql
+++ src/main/resources/db/migration/V2__insert_into_table_video.sql
@@ -1,4 +1,3 @@
-####### video table #######
 INSERT INTO video (id, titulo, descricao, url) VALUES(1, 'Semana 01 | Challenge Back-End',         'Primeiro vídeo do Challenge Backend edição 01',         'https://youtu.be/UiQw2HM4DtM');
 INSERT INTO video (id, titulo, descricao, url) VALUES(2, 'Semana 02 | Challenge Back-End', 'Segundo vídeo do Challenge Backend edição 01', 'https://youtu.be/dq3hZSOcMhI');
 INSERT INTO video (id, titulo, descricao, url) VALUES(3, 'Semana 03 e 04 | Challenge Back-End', 'Terceiro vídeo do Challenge Backend edição 01', 'https://youtu.be/Cw_GxlFnHTg');
